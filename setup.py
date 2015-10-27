d7d2c13786e2e54be54e7e0e5c266d90d5a9384a
Dustin Stansbury <dustin@8tracks.com>
2015-10-26 19:01:18 -0700

    added setup.py, and BaseballReferenceScraper class

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages 
+from codecs import open  # To use a consistent encoding
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+#Get the long description from the relevant file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='umpyre',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Fun with sabermetrics in python',
+    long_description=long_description,    
+    url='https://github.com/dustinstansbury/umpyre',
+    author='Dustin Stansbury',
+    author_email='dustin.stansburay@gmail.com.com',
+    keywords='baseball,sabermetrics,python',
+
+    install_requires=['numpy',
+                      'scipy',
+                      'pandas',
+                      'h5py',
+                      'scikit-learn',
+                      'ipython',
+                      'prettytable',
+                      'MySQL-python',
+                      'statsmodels',
+                      'matplotlib',
+                      'beautifulsoup4',
+                      'dateutils',
+                      'seaborn',
+                      'progressbar'
+                      ],
+)
---
+++ tests/test_baseball_reference_scraper.py
@@ -0,0 +1,1 @@
+from umpyre.data.
